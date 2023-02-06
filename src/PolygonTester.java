075128c5436c4f11f95379fd6166a7ff53ee4d0d
Lqnk4269 <ilanchea@wlhs.wlwv.k12.or.us>
2023-02-06 09:19:15 -0800

    assertions

--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -3,7 +3,9 @@ public class PolygonTester {
         Polygon poly = new Polygon(5, 1);
         System.out.println(poly);
         assert poly.calculatePerimeter() == 5.0;
+        assert String.format("%.4f", poly.calculateArea()).equals("1.7205");
         assert poly.getSideLengths().length == 1;
         assert poly.isValid();
+        assert poly.getShapeType().equals("Pentagon");
     }
 }
